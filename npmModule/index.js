7a1c2dd22784ec61c0fb277bf78312c0001ca5b6
jhautsav <utsavjha2020@gmail.com>
2023-09-22 13:20:53 +0530

    here it self we are adding nodemon and dev and start in script in package.json

--- npmModule/index.js
+++ npmModule/index.js
@@ -2,5 +2,6 @@ const chalk = require('chalk');
 console.log(chalk.blue.inverse('Hello world!')); 
 var validator = require('validator');
 
-let x=validator.isEmail('foo@bar.com............'); 
+let x=validator.isEmail('foo@bar.com'); 
 console.log(x?chalk.blue.inverse(x):chalk.red.inverse(x))
+console.log('hello')
